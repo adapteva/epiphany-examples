1189de5b174929922fc3df4fc044a0e12b9d117a
ysapir <yanivsap@hotmail.com>
2013-08-28 11:36:21 -0400

    Moved the Resource Manager lib from example to erm

--- erm_example/build.sh
+++ erm_example/build.sh
@@ -5,13 +5,15 @@ set -e
 ESDK=${EPIPHANY_HOME}
 ELIBS=${ESDK}/tools/host/lib
 EINCS=${ESDK}/tools/host/include
-ELDF=${ESDK}/bsps/current/fast.ldf
+ERM=../erm
+ERMI=${ERM}/src
+ELDF=${ESDK}/bsps/current/internal.ldf
 
 # Create the binaries directory
 mkdir -p bin/
 
 # Build HOST side application
-gcc src/main.c src/ermlib.c -o bin/main.elf -I ${EINCS} -L ${ELIBS} -le-hal 
+gcc src/main.c ${ERM}/src/ermlib.c -o bin/main.elf -I ${ERMI} -I ${EINCS} -L ${ELIBS} -le-hal
 
 # Build DEVICE side program
 e-gcc -O0 -T ${ELDF} src/e_demo.c -o bin/e_demo.elf -le-lib

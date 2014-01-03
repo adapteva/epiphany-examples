a39a60b07866fd088a353cb3b36543afca41d4bc
aolofsson <andreas@adapteva.com>
2014-01-03 19:57:58 +0000

    Canonical parallel programming example

---
+++ hello_parallella/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+ESDK=${EPIPHANY_HOME}
+ELIBS=${ESDK}/tools/host/lib
+EINCS=${ESDK}/tools/host/include
+ELDF=${ESDK}/bsps/current/fast.ldf
+
+# Create the binaries directory
+mkdir -p bin/
+
+# Build HOST side application
+gcc src/main.c -o bin/main.elf -I ${EINCS} -L ${ELIBS} -le-hal
+
+# Build DEVICE side program
+e-gcc -O3  -T ${ELDF} src/e_task.c -o bin/e_task.elf -le-lib -lm -ffast-math
+
+# Convert ebinary to SREC file
+e-objcopy --srec-forceS3 --output-target srec bin/e_task.elf bin/e_task.srec

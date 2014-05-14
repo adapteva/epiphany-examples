4b6b2e1652254cd0d94f464bc6a6b2636dc3f967
aolofsson <andreas@adapteva.com>
2014-05-14 00:25:16 +0000

    Adding register file storage test to regression suite

---
+++ test/e-regfile-test/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+ESDK=${EPIPHANY_HOME}
+ELIBS=${ESDK}/tools/host/lib
+EINCS=${ESDK}/tools/host/include
+ELDF=${ESDK}/bsps/current/internal.ldf
+
+SCRIPT=$(readlink -f "$0")
+EXEPATH=$(dirname "$SCRIPT")
+cd $EXEPATH
+
+# Create the binaries directory
+mkdir -p bin/
+
+# Build HOST side application
+gcc src/e-regfile-test.c -o bin/e-regfile-test.elf -I ${EINCS} -L ${ELIBS} -le-hal
+
+
---
+++ test/e-regfile-test/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+ESDK=${EPIPHANY_HOME}
+ELIBS=${ESDK}/tools/host/lib:${LD_LIBRARY_PATH}
+EHDF=${EPIPHANY_HDF}
+
+SCRIPT=$(readlink -f "$0")
+EXEPATH=$(dirname "$SCRIPT")
+
+sudo -E LD_LIBRARY_PATH=${ELIBS} EPIPHANY_HDF=${EHDF} $EXEPATH/bin/e-regfile-test.elf $EROW0 $ECOL0 $EROWS $ECOLS > e-regfile-test.log
+
+if [ $? -ne 0 ] 
+then
+    echo "$SCRIPT FAILED"
+else
+    echo "$SCRIPT PASSED"
+fi
+

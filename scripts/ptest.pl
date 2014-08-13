4838796940c2b79747d0c621d4b57904564aa251
aolofsson <andreas@adapteva.com>
2014-08-13 15:47:53 -0400

    Improved test flow (removed enters) Still need to add auto run on boot and end of test summary

--- scripts/ptest.pl
+++ scripts/ptest.pl
@@ -5,14 +5,18 @@
 #system("screen -S demo -X stuff 'date\n'");
 #system("screen -S demo -X stuff 'ls -ltr\n'");
 
-###Get Hostname###
+###Get Hostname/IP address###
 $Host=`hostname`;
 chomp($Host);
+#$IP   = Net::Address::IP::Local->public;
+$IP=`ip addr show eth0 | grep "inet \"`;
+$IP=~ s/(.*?)inet\s+(192.168.3.\d+)\/(.*)/$2/;
+chomp($IP);
 ###SCAN BOARD###
 print "1.) Scan the board bar code:";
 $a = <STDIN>;
 if($a=~ /(.*).*SN(.*).*SKU(.*)/){
-    $MACID=$1;
+    $MACID=lc($1);
     $SN=$2;
     $SKU=$3;
     $DIR=substr($MACID,9,8);
@@ -21,45 +25,58 @@ if($a=~ /(.*).*SN(.*).*SKU(.*)/){
 	system("rm -r Logs/$DIR");
     }
 }
+#print "$MACID $DIR $SN $SKU\n";
+#exit
 
 ###POWER ON BOARD###
-print "2.) Boot the board WITHOUT the SD card inserted, press enter when done: <enter>";
-$a = <STDIN>;
-
+print "2.) Boot the board WITHOUT the SD card inserted.\n";
+system("sleep 5");
 ###FLASH THE BOARD###
 system("screen -s para -X stuff 'printenv\n'");
 system("screen -s para -X stuff 'setenv ethaddr $MACID\n'");
 system("screen -s para -X stuff 'setenv AdaptevaSKU $SKU\n'");
 system("screen -s para -X stuff 'saveenv\n'");
-print "3.) Flashing the board, wait for \"done\" & prompt in 2nd window:<enter>";
-$a = <STDIN>;
-
+print "3.) Flashing the board, wait for \"done\" & prompt in 2nd window.\n";
+print "4.) Reboot the board WITH the SD card inserted.\n";
 ###REBOOT###
-print "4.) Reboot the board WITH the SD card inserted and press enter:<enter>";
-$a = <STDIN>;
 print "5.) Press enter when you see \"root\@linaro-nano:~#\" prompt in 2nd window:<enter>";
 $a = <STDIN>;
 
 ###RUN TEST###
 system("screen -S para -X stuff 'su linaro\n cd\n run\n'");
-system("screen -S para -X stuff 'rcp -r $DIR $Host:Logs\n'");
 
 ###CHECK FOR PASS###
-print "5.) Waiting for test to complete...\n";
-
+print "5.) Did the test complete?(y/n):";
+$a = <STDIN>;
+chomp($a);
+if($a eq "y"){   
+system("screen -S para -X stuff 'rcp -r $DIR $IP:Logs\n'");
 ###DISPLAY PASS/FAIL###
-while(1){  
-    if(-e "Logs/$DIR/PASSED"){
-	system("feh epiphany-examples/scripts/pass.png");
-	exit;
-    }
-    elsif(-e "Logs/$DIR/FAILED"){
+    while(1){  
+	if(-e "Logs/$DIR/PASSED"){
+	    system("feh epiphany-examples/scripts/pass.png");
+	    exit;
+	}
+	elsif(-e "Logs/$DIR/FAILED"){
 	system("feh epiphany-examples/scripts/fail.png");
+	print "What test failed?";
+	$failure = <STDIN>;
+	chomp($failure);
 	exit;
+	}
     }
 }
-    
-
+else{
+    system("screen -S para -X stuff 'rcp -r $DIR $IP:Logs\n'");
+    system("touch Logs/$DIR/FAILED\n");
+    system("feh epiphany-examples/scripts/fail.png");
+    print "What test failed?";
+    $failure = <STDIN>;
+    chomp($failure);
+    exit;
+}
+###COMPILING FILE###
+#system("echo $SN,$MACID $FA    
 
 
 

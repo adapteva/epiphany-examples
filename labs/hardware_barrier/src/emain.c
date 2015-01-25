d3364ddfed648419e5b744ca257699828fac11de
Ola Jeppsson <ola@adapteva.com>
2015-01-25 22:22:01 +0000

    labs/hardware_barrier: Fix race condition
    The hardware barrier example would fail occasionally because of a race
    condition. This commit adds a synchronization flag between the first
    core in the group and the host. When the host sets the flag core0 will
    busy-wait until the flag is cleared. This means that the other cores
    will be idling waiting for the WAND interrupt. But that won't happen
    until all cores in the group issue the WAND instruction. This creates a
    window for the host to read all cores' iteration counters.
    
    This commit also removes old unused ISR assembly code that predates
    Epiphany GCC's interrupt attribute.
    
    * labs/hardware_barrier/src/emain.c (int main): Add
    synchronization point between first E core and host so that
    the host can read out the core iteration counters in a
    race-free manner.
    * labs/hardware_barrier/src/main.c: Likewise.
    * labs/hardware_barrier/src/hwb_isr.s: Remove.
    * labs/hardware_barrier/build.sh: Remove all references to
    hwb_isr.s.
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- labs/hardware_barrier/src/emain.c
+++ labs/hardware_barrier/src/emain.c
@@ -30,15 +30,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <e-lib.h>
 
 #define E_WAND_INT (0x8)
 
 
-void wand_isr(int signum);
+void __attribute__((interrupt)) wand_isr();
 
 unsigned state;
-unsigned *result;
+volatile unsigned *result;
+
+/* Sync point between first core in group and host */
+volatile uint32_t *pause;
 
 
 int main(void) {
@@ -46,22 +50,31 @@ int main(void) {
 	int i;
 	unsigned row, col, delay, num;
 	unsigned *ivt;
-	
+
 	e_irq_global_mask(E_FALSE);
 	e_irq_attach(E_WAND_INT, wand_isr);
 	e_irq_mask(E_WAND_INT, E_FALSE);
-	
+
 	row     = e_group_config.core_row;
 	col     = e_group_config.core_col;
 	num     = row * e_group_config.group_cols + col;
-	result  = (unsigned *) (0x8f000000 + 0x4*num);
-	delay   = 0x10000 * num + 0x10000;
+	pause   = (volatile uint32_t *) (0x7000);
+	result  = (volatile unsigned *) (0x8f000000 + 0x4*num);
+	delay   = 0x2000 * num + 0x2000;
+
+	if (num == 0)
+		*pause = 0;
+
 	*result = 0xdeadbeef;
-	
+
 	for(i=0; i<0x10000; i++)
 	{
 		*result = i;
 		e_wait(E_CTIMER_0, delay);
+
+		if (num == 0)
+			while ((*pause));
+
 
 		__asm__ __volatile__("wand");
 		__asm__ __volatile__("idle");
@@ -71,18 +84,16 @@ int main(void) {
 		state = state & (~0x8);
 		e_reg_write(E_REG_FSTATUS, state);
 	}
-	
+
 	return EXIT_SUCCESS;
 }
 
 
-#if 1
-void __attribute__((interrupt)) wand_isr(int signum)
+void __attribute__((interrupt)) wand_isr()
 {
 //	e_wait(E_CTIMER_1, 0x100);
-	
+
 	return;
 }
-#endif
 
 

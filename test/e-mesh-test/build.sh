4770f94d9df8756f151cb533109612d9fb81868f
Andreas Olofsson <andreas@adapteva.com>
2015-04-04 12:27:29 +0000

    Adding basic mesh test (all to all)

---
+++ test/e-mesh-test/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+ESDK=${EPIPHANY_HOME}
+ELDF=${ESDK}/bsps/current/internal.ldf
+
+SCRIPT=$(readlink -f "$0")
+EXEPATH=$(dirname "$SCRIPT")
+cd $EXEPATH
+
+# Create the binaries directory
+mkdir -p bin
+
+# Build DEVICE side program
+e-gcc -T ${ELDF} ../common/src/test_common.c src/e-task.c -o bin/e-task.elf -le-lib -lm -ffast-math
+
+# Convert ebinary to SREC file
+e-objcopy --srec-forceS3 --output-target srec bin/e-task.elf bin/e-task.srec
---
+++ test/e-mesh-test/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Default values if none are provided
+EROW0=${EROW0:-0}
+ECOL0=${ECOL0:-0}
+EROWS=${EROWS:-4}
+ECOLS=${ECOLS:-4}
+
+SCRIPT=$(readlink -f "$0")
+EXEPATH=$(dirname "$SCRIPT")
+
+
+$EXEPATH/../e-test/bin/e-test.elf $EROW0 $ECOL0 $EROWS $ECOLS 0 $EXEPATH/bin/e-task.srec > e-matmul-test.log
+
+retval=$?
+
+if [ $retval -ne 0 ]
+then
+    echo "$SCRIPT FAILED"
+else
+    echo "$SCRIPT PASSED"
+fi
+
+exit $retval
+
